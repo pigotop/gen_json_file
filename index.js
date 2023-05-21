f2ab8f5d732482ba62f38edf3bc29d1143f6d6b6
MSI\pigot <56533740+pigotop@users.noreply.github.com>
2023-05-21 16:07:34 +0700

    เพิ่มการอ่านข้อมูลจาก csv
    - เพิ่ม csv-parser
    - เปลี่ยนให้จากที่ระบุเองที่ละตัวใน array ให้อ่านข้อมูลจาก csv แทน
    - เพิ่มตัวอย่างของ .env

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 import fs from 'fs-extra';
 import dotenv from 'dotenv';
 import util from 'util';
+import csv from 'csv-parser';
 dotenv.config()
-var prod_cd = [
-    '0502548',
-'0502549',
-'6200815',
-];
-var jsondata = [];
-prod_cd.forEach(element => {
-    let jsdata = { PROD_CD: element , INV_MVMNT_QTY: process.env.NUM_INV_MVMNT_QTY };
-    jsondata.push(jsdata)
+
+let prod_cd = [];
+let jsondata = [];
+
+fs.createReadStream("data.csv")
+.pipe(csv())
+.on('data', (data) => {
+    console.log("jsondata =>",data);
+    prod_cd.push(data.prod_cd)
+})
+.on('end', ()=> {
+    console.log("END READ");
+    console.log("prod_cd =>",prod_cd);
+
+    prod_cd.forEach(element => {
+        let jsdata = { PROD_CD: element , INV_MVMNT_QTY: process.env.NUM_INV_MVMNT_QTY };
+        jsondata.push(jsdata)
+    });
+    
     console.log(jsondata)
-});
+    fs.removeSync('./output');
+    fs.outputJsonSync('./output/'+genJsonFileName(),jsondata);
+    console.log("========== finish ==========");
+    
+})
 
-fs.removeSync('./output');
-fs.outputJsonSync('./output/'+genJsonFileName(),jsondata);
 
-console.log("========== finish ==========");
 
 function genJsonFileName(){
     const today = new Date();
